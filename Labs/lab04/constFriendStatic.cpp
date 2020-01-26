611ed5ebcd3bc55dde1d014e15d40b1cd963974d
hectorlopez390 <53275112+hectorlopez390@users.noreply.github.com>
2020-01-25 22:03:47 -0800

    Update constFriendStatic.cpp

--- Labs/lab04/constFriendStatic.cpp
+++ Labs/lab04/constFriendStatic.cpp
@@ -44,5 +44,36 @@ int main()
   return 0;
 }
 
-
 #endif //CONSTFRIENDSTATIC_CPP
+
+/*
+Output:
+MacBook-Pro-3:lab4 hector$ g++ Employee.cpp constFriendStatic.cpp
+MacBook-Pro-3:lab4 hector$ ./a.out
+
+Name: Unknown
+Office Number: 10
+Employee Number: 1000
+Department Number: 0
+Employee Position: E
+Years of Experience: 0
+Salary: 0
+Total Number of Employees: 1
+Next Employee Number: 1001
+Next Office Number: 11
+
+Enter employee name: Adam Smith
+Enter employee position: P
+Enter years of experience: 7
+
+Name: Adam Smith
+Office Number: 11
+Employee Number: 1001
+Department Number: 10
+Employee Position: P
+Years of Experience: 7
+Salary: 65000
+Total Number of Employees: 2
+Next Employee Number: 1002
+Next Office Number: 12
+*/
