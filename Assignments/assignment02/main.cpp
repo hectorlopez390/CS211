b0a388072b5153b1c5489c13cd06f169cd4da29c
hectorlopez390 <53275112+hectorlopez390@users.noreply.github.com>
2020-05-24 23:40:33 -0700

    Update main.cpp

--- Assignments/assignment02/main.cpp
+++ Assignments/assignment02/main.cpp
@@ -18,3 +18,45 @@ int main()
 }
 
 #endif // MAIN_CPP
+
+/*
+Output:
+MacBook-Pro-3:assignment2 hector$ g++ main.cpp Dictionary.cpp
+MacBook-Pro-3:assignment2 hector$ ./a.out
+Enter a transaction file: transactionFile.txt
+"student" added to dictionary.
+"teacher" added to dictionary.
+"cs211" added to dictionary.
+Processing words from file "data.txt" to insert into Dictionary...
+Words from file "data.txt" processed successfully.
+Printing file S.txt in dictionary...
+student systems successful suffer sufficiently 
+semantics system static show second 
+serializability support 
+"student" deleted from dictionary.
+"hello" added to dictionary.
+Spell checking file "myEssay.txt"...
+Spelling error. "concrrent" not found in dictionary.
+Spelling error. "themeselves" not found in dictionary.
+Spelling error. "diferent" not found in dictionary.
+Spelling error. "wich" not found in dictionary.
+Spelling error. "analyziz" not found in dictionary.
+Spelling error. "enhanc" not found in dictionary.
+Spelling error. "transactian" not found in dictionary.
+Spelling error. "relatted" not found in dictionary.
+Cannot delete "play" because word does not exist in dictionary.
+Searching for "medical" in dictionary...
+"medical" not found in dictionary.
+Searching for "cs211" in dictionary...
+"cs211" found in dictionary.
+Printing file C.txt in dictionary...
+cs211 current concurrency changes consistency 
+computationally context correctness criterion conflict 
+control 
+Printing file H.txt in dictionary...
+have has how histories hello 
+Printing file T.txt in dictionary...
+teacher to transaction that typically 
+the transactions themselves this these 
+than tractable third they 
+*/
