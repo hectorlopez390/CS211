f99ced2bf33ef6eb00b1e3a50c906388f4c3e3e4
hectorlopez390 <53275112+hectorlopez390@users.noreply.github.com>
2020-06-04 00:24:09 -0700

    Update University.h

--- Assignments/assignment03/University.h
+++ Assignments/assignment03/University.h
@@ -28,20 +28,15 @@ class University
   bool checkIfFacultyExists(long facultyId);
 
   bool CreateNewDepartment(string depName, string depLoc, long depChairId);
-  //bool RemoveADepartment(long depId);
   bool CreateNewStudent(string sName, string sEmail, string sAddress, string sDateOfBirth, string sGender, int sYearOfStudy, string sMajor, long sAdvisorId);
-  //bool RemoveAStudent(long sStId); 
   bool CreateNewCourse(string cName, long cDepId, long cTaughtBy, int cMaxSeat);
-  //bool RemoveACourse(long cCRN);
   bool CreateNewFaculty(string fName, string fEmail, string fAddress, string fDateOfBirth, string fGender, float fSalary, int fYearOfExp, long fDepId);
-  //bool RemoveAFaculty(long fFactId);
   bool ListCoursesTaughtByFaculty(long facultyId);
   bool ListCoursesTakenByStudent(long studentId);
   bool ListFacultiesInDepartment (long departId);
   bool ListStudentsOfAFaculty(long facultyId);
   bool ListCoursesOfADepartment(long departId);
   bool AddACourseForAStudent(long courseId, long stId);
-  //bool DropACourseForAStudent(long courseId, long stId);
   bool AssignDepartmentChair(long facultyId, long departId);
   bool AssignInstructorToCourse (long facultyId, long courseId);
   
